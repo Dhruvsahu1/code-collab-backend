cc1a00a7e16a39aaddf4098ed922cf8e4db54d9c
Dhruv <dhruvsahu7355@gmail.com>
2026-04-07 12:48:35 +0530

    [Dhruv] Add. added the editor-service microservice

---
+++ file-service/src/main/java/com/codesync/file/feign/ProjectClient.java
@@ -0,0 +1,13 @@
+package com.codesync.file.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "project-service", url = "${app.services.project-service}")
+public interface ProjectClient {
+
+    @GetMapping("/projects/{id}")
+    ResponseEntity<Object> getProject(@PathVariable("id") Long projectId);
+}
---
+++ file-service/src/main/java/com/codesync/file/service/FileService.java
@@ -0,0 +1,33 @@
+package com.codesync.file.service;
+
+import com.codesync.file.dto.*;
+import com.codesync.file.entity.CodeFile;
+
+import java.util.List;
+
+public interface FileService {
+
+    CodeFile createFile(CreateFileRequest request);
+
+    CodeFile getFileById(Long fileId);
+
+    List<CodeFile> getFilesByProject(Long projectId);
+
+    String getFileContent(Long fileId);
+
+    CodeFile updateFileContent(Long fileId, UpdateContentRequest request);
+
+    CodeFile renameFile(Long fileId, String newName);
+
+    void deleteFile(Long fileId);
+
+    void restoreFile(Long fileId);
+
+    CodeFile moveFile(Long fileId, String newPath);
+
+    CodeFile createFolder(CreateFolderRequest request);
+
+    List<FileTreeNode> getFileTree(Long projectId);
+
+    List<CodeFile> searchInProject(Long projectId, String keyword);
+}
